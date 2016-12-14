7101bf9bb8cd23b56e088c88276245353ba35c9d
root <moibonill@hotmail.com>
2016-12-13 21:53:24 -0500

    SCRIPT -  Formateo de Datos a utf-8, correccion de creacion de carpeta datos_csv en script/

--- problema1/problema1.sh
+++ problema1/problema1.sh
@@ -4,7 +4,12 @@ DATA=/root/Proyecto-de-Programacion/problema1/hojasDatos
 mkdir $DATA/datos_csv
 
 
-OUT_DATA=$DATA/archivos_csv
+OUT_DATA=$DATA/datos_csv
+GRAF_DATA=$DATA/datos_graf
+
+mkdir $GRAF_DATA
+
+
 m=0
 
 for i in `find $DATA -name '*.xls' ` 
