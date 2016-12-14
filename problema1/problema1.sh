849ded3b8128220c8085609d84ccbaedc2afd366
root <moibonill@hotmail.com>
2016-12-13 19:50:37 -0500

    SCRIPT -  Lineas finalizadas , se espera que el archivo error1.log sea lleno con información descriptiva del mismo

--- problema1/problema1.sh
+++ problema1/problema1.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-DATA=/root/Proyecto-de-Programacion/problema1/hojasDatos
+DATA=/root/Proyecto-de-Programacion/pboblema1/hojasDatos
 mkdir $DATA/datos_csv
+
+
+OUT_DATA=$DATA/archivos_csv
+m=0
+
+for i in `find $DATA -name '*.xls' ` 
+do        
+      echo "Procesando archivo $i"
+      xls2csv $i > $OUT_DATA/data-$m.csv
+      let m=m+1
+
+done 2> error1.log
